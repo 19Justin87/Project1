3a2015fed9eb9e6bb1349209cda960d490fead9a
Justin Snipes <snipesatl258@gmail.com>
2019-08-23 15:04:39 -0700

    updated files

--- js/script.js
+++ js/script.js
@@ -1,56 +1,97 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// An array
+var quotes = [
+ 
+  // objects in arrey
+    {
+      quote: "Isn't it funny how day by day nothing changes but when you look back everything is different.",
+      source: "C.S. Lewis",
+      citation: "Prince Caspian",
+      year: 1951,
+    },
+    {
+      quote: "All that we are is the result of what we have thought.",
+      source: "Budda",
+      citation: "Religion",
+      year: "Unknown",
+    },
+    {
+      quote: "I have no special talent. I am only passionately curious.",
+      source: "Albert Einstein",
+      citation: "Talent",
+      year: "Unknown",
+    },
+    {
+      quote: "Wisely, and slow. They stumble that run fast.",
+      source: "William Shakespeare",
+      citation: "Wisdom",
+      year: "Unknown",
+    },
+    {
+      quote: "Stay hungry, stay foolish.",
+      source: "Steve Jobs",
+      citation: "Ambition",
+      year: "Unknown",
+    },
+    {
+      quote: "The future belongs to those who prepare for it today.",
+      source: "Malcolm X",
+      citation: "Future",
+      year: "Unknown",
+    },
+    {
+      quote: "The successful warrior is the average man, with laser-like focus.",
+      source: "Bruce Lee",
+      citation: "Success",
+      year: "Unknown",
+    },
+    {
+      quote: "Try not to become a man of success but rather to become a man of value.",
+      source: "Albert Einstein",
+      citation: "Life",
+      year: "Unknown",
+    },
+    {
+      quote: "All get what they want; they do not always like it",
+      source: "C.S. Lewis",
+      citation: "The Magician's Nephew",
+      year: 1955,
+    },
+    {
+      quote: "Nothing that you have not given away will ever be really yours",
+      source: "C.S. Lewis",
+      citation: "Mere Christianity",
+      year: 1952,
+    },
+  
+  ];
+  
+  // Function getRandomQuote array to return random quote value
+  function getRandomQuote(array) {
+    
+    var quoteSearch = Math.floor(Math.random() * (quotes.length));
+    for (var i = 0; i < array.length; i++) {
+        var randomQuote = array[quoteSearch];
+    }
+    return randomQuote;
+  }
+  
+  // Create the printQuote funtion to layout quotes
+  function printQuote() {
+         
+    var anwser = getRandomQuote(quotes);
+    var message = "";
+      message = "<p class='quote'>" + anwser.quote + "</p>";
+      message +="<p class='source'>" + anwser.source;
+      message += "<span class='citation'>" + anwser.citation + "</span>";
+      message +="<span class='year'>" + anwser.year + "</span>"
+      message += "</p>";
+  
+    document.getElementById('quote-box').innerHTML = message;
+  }
+ 
+  // Print above code
+  printQuote(); 
+  
+  
+  // Click to show another quote
+  document.getElementById('loadQuote').addEventListener("click", printQuote, false);  
